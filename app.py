2b3443beb4314b63699629bc99c8d29552773be5
Giya001 <giyoshikmatillayev@gmail.com>
2025-06-10 21:33:59 +0500

    Update app.py

--- app.py
+++ app.py
@@ -8,39 +8,20 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
-from dotenv import load_dotenv
 
-# Bot token can be obtained via https://t.me/BotFather
+from dotenv import load_dotenv
 load_dotenv()
 TOKEN = getenv("BOT_TOKEN")
-
-
-
 dp = Dispatcher()
 
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-
     await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
 
 
-@dp.message()
-async def echo_handler(message: Message) -> None:
-
-    try:
-        # Send a copy of the received message
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        # But not all the types is supported to be copied so need to handle it
-        await message.answer("Nice try!")
-
-
 async def main() -> None:
-    # Initialize Bot instance with reply bot properties which will be passed to all API calls
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-
-    # And the run events dispatching
     await dp.start_polling(bot)
 
 
